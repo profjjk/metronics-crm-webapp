3a1e0623d94181e1e51a5fc4f64e6f4d414d72bf
Jordan Kelly <63018389+profjjk@users.noreply.github.com>
2022-01-08 11:39:47 -0800

    organize backend into server folder

--- models/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Customer: require('./customer'),
-    Job: require('./job'),
-    Part: require('./part'),
-    User: require('./user'),
-    Request: require('./request'),
-    Message: require('./message')
-};
