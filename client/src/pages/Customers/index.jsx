96520c7fd9ebc88fc812667b55439a0da7070e18
Jordan Kelly <63018389+profjjk@users.noreply.github.com>
2022-01-15 01:24:38 -0800

    reorganize style

--- client/src/pages/Customers/index.jsx
+++ client/src/pages/Customers/index.jsx
@@ -2,6 +2,7 @@ import { Redirect } from 'react-router-dom';
 import { useData, useCustomers, useUser, useJobs } from '../../react-query';
 import { Header, CustomersTable, CustomerHistory, CustomerForm } from './sections';
 import { ServiceForm } from '../Service/sections';
+import './style.scss';
 
 const CustomerPage = () => {
     const { user } = useUser();
