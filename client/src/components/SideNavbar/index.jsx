9d64ef62fa67b46267f25e8f7fd9ee1153e50659
Jordan Kelly <jordankelly@Jordans-MacBook-Pro.local>
2022-02-18 12:28:29 -0800

    refactor navbar

--- client/src/components/SideNavbar/index.jsx
+++ client/src/components/SideNavbar/index.jsx
@@ -1,95 +1,51 @@
-import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useQueryClient } from 'react-query';
-import { useAuth, useData } from '../../react-query';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../react-query';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUserFriends, faTools, faWarehouse, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import './style.scss';
 
 const SideNavbar = () => {
-    const qc = useQueryClient()
-    const { logout } = useAuth();
-    const history = useHistory();
-    const [page, setPage] = useState("");
-    const view = useData('view');
+	const { logout } = useAuth();
 
-    useEffect(() => {
-        const path = window.location.pathname;
-        setPage(path)
-    }, [view]);
+	const setActiveLink = e => {
+		e.preventDefault();
 
-    useEffect(() => {
-        const links = document.querySelectorAll('svg')
-        for (let link of links) {
-            link.classList.remove('active');
-        }
+		const links = document.querySelectorAll('svg')
+		for (let link of links) {
+			link.classList.remove('active');
+		}
 
-        switch (page) {
-            case '/dashboard':
-                links[0].classList.add('active');
-                break;
-            case '/customers':
-                links[1].classList.add('active');
-                break;
-            case '/service':
-                links[2].classList.add('active');
-                break;
-            case '/inventory':
-                links[3].classList.add('active');
-                break;
-        }
-    }, [page]);
+		e.currentTarget.children[0].children[0].classList.add('active');
+	}
 
-    // EVENT LISTENERS
-    const signOut = () => {
-        logout();
-        history.push('/');
-        window.location.reload();
-    }
+	const links = [
+		{ path: '/dashboard', icon: faHome },
+		{ path: '/customers', icon: faUserFriends },
+		{ path: '/service', icon: faTools },
+		{ path: '/inventory', icon: faWarehouse }
+	]
 
-    return (
-        <header>
-            <nav className={"navbar"}>
-                <ul>
-                    <li className={"nav-item"} onClick={() => {
-                        qc.setQueryData('view', 'default');
-                        setPage('/dashboard');
-                        history.push('/dashboard');
-                    }}>
-                        <FontAwesomeIcon icon={faHome}/>
-                    </li>
+	return (
+		<header>
+			<nav className={'navbar'}>
+				<ul>
+					{links.map(({ path, icon }) => (
+						<li key={path} onClick={setActiveLink}>
+							<Link className={'nav-item'} to={path}>
+								<FontAwesomeIcon icon={icon}/>
+							</Link>
+						</li>
+					))}
 
-                    <li className={"nav-item"} onClick={() => {
-                        qc.setQueryData('view', 'default');
-                        setPage('/customers');
-                        history.push('/customers');
-                    }}>
-                        <FontAwesomeIcon icon={faUserFriends}/>
-                    </li>
-
-                    <li className={"nav-item"} onClick={() => {
-                        qc.setQueryData('view', 'default');
-                        setPage('/service');
-                        history.push('/service');
-                    }}>
-                        <FontAwesomeIcon icon={faTools}/>
-                    </li>
-
-                    <li className={"nav-item"} onClick={() => {
-                        qc.setQueryData('view', 'default');
-                        setPage('/inventory');
-                        history.push('/inventory');
-                    }}>
-                        <FontAwesomeIcon icon={faWarehouse}/>
-                    </li>
-                </ul>
-
-                <button className={"signOut"} onClick={signOut}>
-                    <FontAwesomeIcon icon={faSignOutAlt}/>
-                </button>
-            </nav>
-        </header>
-    )
+					<li onClick={logout}>
+						<Link className={'signOut'} to={'/login'}>
+							<FontAwesomeIcon icon={faSignOutAlt}/>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
 }
 
 export default SideNavbar;
